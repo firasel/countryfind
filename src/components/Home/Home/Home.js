934df9ccef631f0391d7ef0c85acdb973f617d5d
firasel <mdrasel601390@gmail.com>
2021-09-22 20:53:41 +0600

    project setup complete

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            This is Home Page
+        </div>
+    );
+};
+
+export default Home;
